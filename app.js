255a7c3ddc299a7290b1a058e3e0c76cc1cf5f2c
44502699 <68951572+seong-94@users.noreply.github.com>
2022-06-17 16:19:52 +0900

    2.4 Change range
    Can change range of brush size

--- app.js
+++ app.js
@@ -1,55 +1,79 @@
-const canvas = document.getElementById("jsCanvas"); //html5 
+const canvas = document.getElementById("jsCanvas"); //html5
 const ctx = canvas.getContext("2d"); // pixel coordinates
 const colors = document.getElementsByClassName("jsColors");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 500;  
+canvas.width = 500;
 canvas.height = 500;
 
 ctx.strokeStyle = "black"; // line color
 ctx.lineWidth = 2.5; // line width
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
-    painting = false;
+  painting = false;
 }
 function startPainting() {
-    painting = true;
+  painting = true;
 }
 function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if (!painting) {
-        ctx.beginPath();    // start a new path(is a line)
-        ctx.moveTo(x, y);
-    } else {
-        ctx.lineTo(x, y); // Create line 
-        ctx.stroke(); // draw the line (every time)
-    }
-
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath(); // start a new path(is a line)
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y); // Create line
+    ctx.stroke(); // draw the line (every time)
+  }
 }
 function onMouseDown(event) {
-    painting = true;
+  painting = true;
 }
 
 function onMouseUp(event) {
-    stopPainting();
+  stopPainting();
 }
 
 function handleColorChange(event) {
-    const color = event.target.style.backgroundColor;
-    ctx.strokeStyle = color;
-    // console.log(color);
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  // console.log(color);
 }
-
 
 if (canvas) {
-    canvas.addEventListener("mousemove", onMouseMove);// offset X and Y is canvas position
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mousemove", onMouseMove); // offset X and Y is canvas position
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
 
-Array.from(colors).forEach(color => 
-    color.addEventListener("click", handleColorChange)
-    );
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorChange)
+);
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+function handleModeChange(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeChange);
+}
