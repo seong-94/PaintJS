171be174613fdaa045f8f62c3a4410ab610fd05a
44502699 <68951572+seong-94@users.noreply.github.com>
2022-06-17 15:52:14 +0900

    2.3 Changing colour
    Add Change color eventlistener

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("jsCanvas"); //html5 
+const ctx = canvas.getContext("2d"); // pixel coordinates
+const colors = document.getElementsByClassName("jsColors");
 
-canvas.width = 500;
+canvas.width = 500;  
 canvas.height = 500;
 
-ctx.strokeStyle = "black";
-ctx.lineWidth = 2.5;
+ctx.strokeStyle = "black"; // line color
+ctx.lineWidth = 2.5; // line width
 
 let painting = false;
 
@@ -19,11 +20,11 @@ function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     if (!painting) {
-        ctx.beginPath();
+        ctx.beginPath();    // start a new path(is a line)
         ctx.moveTo(x, y);
     } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+        ctx.lineTo(x, y); // Create line 
+        ctx.stroke(); // draw the line (every time)
     }
 
 }
@@ -35,6 +36,12 @@ function onMouseUp(event) {
     stopPainting();
 }
 
+function handleColorChange(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    // console.log(color);
+}
+
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);// offset X and Y is canvas position
@@ -42,3 +49,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorChange)
+    );
