9e7249e2f3f231377bba6acd7cbe0e77d9c7e5cf
44502699 <68951572+seong-94@users.noreply.github.com>
2022-06-17 11:14:22 +0900

    2.1 2D Context
    Add canvas context
    Draw line (not works)

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const  canvas =  document.getElementById("jsCanvas");
-
-
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "black";
 let painting = false;
 
 function stopPainting() {
     painting = false;
-    
+}
+function startPainting() {
+    painting = true;
 }
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else { 
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 
 }
 function onMouseDown(event) {
-    painting = true;
+    startPainting();
 }
 
 function onMouseUp(event) {
@@ -22,9 +30,9 @@ function onMouseUp(event) {
 }
 
 
-if(canvas){
+if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);// offset X and Y is canvas position
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", stopPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
